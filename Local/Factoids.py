78f7b46525981a38996310297fbad8f76f1399f5
Tim Meusel <tim@bastelfreak.de>
2016-12-09 21:34:48 +0100

    honor google pep guides

--- Local/Factoids.py
+++ Local/Factoids.py
@@ -1,19 +1,33 @@
-"""Simple file to deal with static factoids"""
 import logging
 from Legobot.Lego import Lego
 
 logger = logging.getLogger(__name__)
 
 class Factoids(Lego):
-    """Class to hold all factoids"""
+    """Class to hold all factoids
+    Args: Lego
+    """
     def listening_for(self, message):
-        """Checks if the message contains a command that we need to execute"""
+        """Checks if the message contains a command that we need to execute
+        Args:
+            self:
+            message: The complete line/message that comes from an IRC channel
+
+        Returns:
+            Bool: Returns true if the first word in the message is a command for this class
+        """
         cmds = ['!shrug', '!tableflip', '!nope', '!doit', '!wat', '!@', '!source', '!deal', \
                 '!awesome', '!nuke', '!stats', '!docs']
         return message['text'].split()[0] in cmds
 
     def handle(self, message):
-        """Execute the needed command"""
+        """Execute the needed command
+        Args:
+            self:
+            message: The complete line/message that comes from an IRC channel
+        
+        Returns:
+            string: Returns the suitable factoid"""
         opts = None
         logger.info(message)
         try:
@@ -51,11 +65,24 @@ class Factoids(Lego):
 
 
     def get_name(self):
-        """Returns the name of this class"""
+        """Returns the name of this class
+        Args:
+            self:
+
+        Returns:
+            string: The name of this class
+        """
         return 'factoids'
 
     def get_help(self):
-        """Prints a useful help message into the channel"""
+        """Prints a useful help message into the channel
+        
+        Args:
+            self:
+                
+        Returns:
+            String: A help message that explains this class
+        """
         help_text = "collection of nice factoids (static reponses). " \
                 "Usage: !shrug, !tableflip, !nope, !doit, !wat, !@, !source, !deal, !awesome, " \
                 "!nuke, !stats, !docs "
