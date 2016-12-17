3bcc5b29af29570979450d5e5835ec254d48b6b9
Bren Briggs <briggs.brenton@gmail.com>
2016-12-17 22:07:53 +0000

    Line breaks, better help text.

--- Local/Msync.py
+++ Local/Msync.py
@@ -3,6 +3,7 @@ import logging
 import semver
 import re
 import json
+from datetime import date
 from Legobot.Lego import Lego
 
 logger = logging.getLogger(__name__)
@@ -16,7 +17,8 @@ class Audit(Lego):
     def handle(self,message):
         try:
             target = message['metadata']['source_channel']
-            opts = {'target':target}
+            self.opts = {'target':target}
+            self.message = message
         except IndexError:
             logger.error('Could not identify message source in message: %s' \
                          % str(message))
@@ -25,19 +27,29 @@ class Audit(Lego):
         dispatcher = {'getver':self._get_single_version,
                      'olderthan':self._compare_to_version,
                      'newerthan':self._compare_to_version,
-                     'current':self._compare_to_version}
+                     'current':self._compare_to_version,
+                     'version':self._get_current_msync}
 
         args = message['text'].split()
-        command = args[1]
+        if len(args) > 1:
+            command = args[1]
+        else:
+            command = 'version'
+        logger.debug('%s requested. Dispatching %s with args: %s' % (command,
+                                                                     str(dispatcher[command]),
+                                                                     str(args)))
         response = dispatcher[command](*args)
-        self.reply(message,str(response),opts)
+        # Returning things other than a str to chat really freaks legobot out.
+        # Someone should fix that.
+        self.reply(message,str(response),self.opts)
 
     def get_name(self):
         return 'msync'
 
     def get_help(self):
-        return 'Discover information about the status of modulesync on managed\
- repositories. Usage: !msync [getver modulename].'
+        return 'Discover information about the status of modulesync on managed '\
+                'repositories. Usage: !msync. Fore more info, see the docs at '\
+                'https://github.com/voxpupuli/thevoxfox/tree/master/docs/msync.md'
 
     def _get_current_msync(self,*args,**kwargs):
         try:
@@ -63,7 +75,8 @@ class Audit(Lego):
                 modules = all_modules.text
                 modules = modules.split('\n')
                 modules = [module.strip() for module in modules]
-                modules = [module for module in modules if module not in yaml_artifacts]
+                modules = [module for module in modules if \
+                           module not in yaml_artifacts]
                 modules = [module[2:] for module in modules]
                 return modules
         except:
@@ -83,16 +96,23 @@ class Audit(Lego):
         try:
             status = args[1]
         except IndexError:
-            logger.error('msync._compare_to_version() called without enough arguments!')
+            logger.error('msync._compare_to_version() called '\
+                         'without enough arguments!')
             return "Not enough arguments!"
 
         # Now the fun begins
+        working_prompt = 'Fetching and comparing against all our modules. '\
+                'This takes a second...'
+        self.reply(self.message,str(working_prompt),self.opts)
         all_modules = self._get_all_modules()
         matching_modules = []
         if status == 'current':
             current_msync = self._get_current_msync()
             for module in all_modules:
-                modver = self._get_single_version('','',module) # Workaround until **kwargs is ironed out
+                # Use a requests Session to take advantage of connection pooling
+                s = requests.Session()
+                kwargs = {'module':module,'session':s}
+                modver = self._get_single_version(**kwargs) # Workaround until **kwargs is ironed out
                 # Sometimes we get 404s. ¯\_(ツ)_/¯
                 if modver != False:
                     if semver.compare(modver,current_msync) == 0: # semvers matched
@@ -141,23 +161,29 @@ class Audit(Lego):
 
     def _get_single_version(self,*args,**kwargs):
         # For the love of God, check for length!
-        if 'modname' not in kwargs:
+        logger.debug(kwargs)
+        if 'module' not in kwargs:
             try:
-                modname = args[2]
+                module = args[2]
             except IndexError:
                 logger.error('Not enough arguments supplied to _get_single_version()')
                 return "Not enough arguments!"
-        msync_ver = requests.get(raw_url + modname + '/master/.msync.yml')
+        else: 
+            module = kwargs['module']
+        msync_ver = requests.get(raw_url + module + '/master/.msync.yml')
         if msync_ver.status_code == requests.codes.ok:
             msync_ver = msync_ver.text
             msync_ver = re.search('\d+\.\d+\.\d+', msync_ver)
             return msync_ver.group(0)
         else:
-            logger.debug('Got a bad return code from module %s: %s' % (modname,msync_ver.status_code))
+            logger.debug('Got a bad return code from module %s: %s' % (module,msync_ver.status_code))
             return '0.0.0'
 
     def _gist(self,text):
         # input is a list. we want one module per line
+        timestamp = date.today()
+        header = '# Report generated by TheVoxFox on %s' % timestamp.strftime("%A, %d %B %Y")
+        text.insert(0,header) 
         text = '\n'.join(text)
         body = {'descrption':'Voxpupuli modulesync report',
                 'files':{
@@ -170,4 +196,7 @@ class Audit(Lego):
         logger.debug('_gist response code: %s' % r.status_code)
         r = r.json()
         logger.debug(r)
-        return r['html_url']
+        if 'html_url' in r:
+            return r['html_url']
+        else:
+            return "Unable to generate a gist at this time..."
